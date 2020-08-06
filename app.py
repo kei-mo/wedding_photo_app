161ab1f613e15d458ab0b1cf94f8ba42a9892d33
jingfang zhou <jingfang95@yahoo.co.jp>
2020-08-06 22:15:04 +0900

    update

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
-
-
 import os
 # request フォームから送信した情報を扱うためのモジュール
 # redirect  ページの移動
 # url_for アドレス遷移
-from flask import Flask, request, redirect, url_for, render_template
+from flask import Flask, request, redirect, url_for, render_template, current_app
 # ファイル名をチェックする関数
 from werkzeug.utils import secure_filename
 # 画像のダウンロード
 from flask import send_from_directory
+from livereload import Server, shell
+import numpy as np
+from helper import get_hsv_from_path, get_hsv_info
+import pickle
 
 # 画像のアップロード先のディレクトリ
 UPLOAD_FOLDER = './uploads'
@@ -16,9 +18,22 @@ UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'gif'])
 
 app = Flask(__name__)
+# remember to use DEBUG mode for templates auto reload
+# https://github.com/lepture/python-livereload/issues/144
+app.debug = True
+# print(app.wsgi_app) -> <bound method Flask.wsgi_app of <Flask 'app'>>
+server = Server(app.wsgi_app)
 
+server.watch('./uploads', )
+# server.serve(watch)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# global constant
+hue_constant = 18
+pixel_resolution = 30
+
+# global variable
+hue_block = []
 
 def allwed_file(filename):
     # .があるかどうかのチェックと、拡張子の確認
@@ -47,11 +62,67 @@ def uploads_file():
             filename = secure_filename(file.filename)
             # ファイルの保存
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            with open("target.npy", "rb") as f:
+                np_im = np.load(f)
+            try:
+                with open("hue_block.pkl", "rb") as f:
+                    hue_block = pickle.load(f)
+            except:
+                raise Exception("perhaps no pkl file.")
+            hsv = get_hsv_from_path(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            # TODO:target.npyの更新
+            # 画像のhue, sat, briを計算
+            # 正方形にする
+            # hue_blockの対象カテゴリを最新の画像で置換
             # アップロード後のページに転送
+
+            img = Image.fromarray(np_im)
             return render_template("index.html")
-            # hey
-            # return redirect(url_for('uploaded_file', filename=filename))
-    return render_template("index.html")
+    if request.method == 'GET':
+        print(current_app)
+        print(dir(current_app))
+        # target.npyを画像にする下に手渡す
+        return render_template("index.html")
+
+@app.route('/admin', methods=['GET', 'POST'])
+def set_target():
+    # リクエストがポストかどうかの判別
+    if request.method == 'POST':
+        # ファイルがなかった場合の処理
+        if 'file' not in request.files:
+            flash('ファイルがありません')
+            return redirect(request.url)
+        # データの取り出し
+        file = request.files['file']
+        # ファイル名がなかった時の処理
+        if file.filename == '':
+            flash('ファイルがありません')
+            return redirect(request.url)
+        # ファイルのチェック
+        if file and allwed_file(file.filename):
+            # 危険な文字を削除（サニタイズ処理）
+            filename = secure_filename(file.filename)
+            # ファイルの保存
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], "original_target.jpg"))
+            hsv = get_hsv_from_path(os.path.join(app.config['UPLOAD_FOLDER'], "original_target.jpg"))
+            with open("target.npy", "wb") as f:
+                np.save(f, hsv)
+            # target.npyを使ってhue_blockの作成
+            max_0dim = hsv.shape[0] // pixel_resolution
+            max_1dim = hsv.shape[1] // pixel_resolution
+            hue_block = [[] for _ in range(hue_constant)]
+            for i in range(max_0dim):
+                for j in range(max_1dim):
+                    block_hsv = hsv[i*pixel_resolution : (i + 1) * pixel_resolution, \
+                                    j*pixel_resolution : (j + 1) * pixel_resolution, :]
+                    hue_category, avg_s, avg_v = get_hsv_info(block_hsv, hue_constant)
+                    hue_block[hue_category].append((i,j,avg_s,avg_v))
+            with open("hue_block.pkl", "wb") as f:
+                pickle.dump(hue_block, f)
+
+            return redirect(url_for("uploads_file"))
+    return render_template("admin.html")
 
 @app.route('/uploads/<filename>')
 # ファイルを表示する
---
+++ helper.py
@@ -0,0 +1,31 @@
+from PIL import Image
+from numpy import asarray
+import cv2
+from scipy import stats
+import numpy as np
+def get_hsv_from_path(path):
+    image  = Image.open(path)
+    data = asarray(image)
+    hsv = cv2.cvtColor(data, cv2.COLOR_BGR2HSV)
+    return hsv
+
+def get_hsv_info(hsv, hue_constant: int):
+    size_3d_array = hsv.shape
+    total_pixel = size_3d_array[0]*size_3d_array[1]	# ピクセル数
+    hue_category_list = []		# 各ピクセルのhue_categoryをリストで記憶
+    s = 0
+    v = 0
+    denom = 180 / hue_constant
+    for i in range(size_3d_array[0]):
+        for j in range(size_3d_array[1]):
+            hsv_px = hsv[i,j,:]
+            s = s + hsv_px[1]	# sをたす
+            v = v + hsv_px[2]	# vをたす
+            hue_px = hsv_px[0]
+            hue_category_px = hue_px // denom
+            hue_category_list.append(hue_category_px)	# hue_categoryのリストを追加
+    avg_s = s/total_pixel
+    avg_v = v/total_pixel
+    array_hue_category_list = np.array(hue_category_list)
+    hue_category = stats.mode(array_hue_category_list)		
+    return hue_category, avg_s, avg_v
